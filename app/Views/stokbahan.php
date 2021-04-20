29061cdc26fe77776d59dbc559964bf0a583fd16
davidarment1421 <64887984+davidarment1421@users.noreply.github.com>
2021-04-21 01:12:31 +0700

    Add files via upload

--- app/Views/stokbahan.php
+++ app/Views/stokbahan.php
@@ -6,23 +6,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Stok Bahan</title>
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
 </head>
 
-<body style="width: 70%; margin: 0 auto; padding-top: 30px;">
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Stok Bahan </h2>
-            </div>
-        </div>
+<body>
+
+    <div class="container mt-5 mb-5 text-center">
+        <h4>Stok Bahan</h4>
     </div>
-    <hr>
-    <!--fungsi read data-->
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
+    <div class="container">
+        <?php
+        if (!empty(session()->getFlashdata('success'))) { ?>
+
+            <div class="alert alert-success">
+                <?php echo session()->getFlashdata('success'); ?>
+            </div>
+
+        <?php } ?>
+        <?php if (!empty(session()->getFlashdata('info'))) { ?>
+
+            <div class="alert alert-info">
+                <?php echo session()->getFlashdata('info'); ?>
+            </div>
+
+        <?php } ?>
+
+        <?php if (!empty(session()->getFlashdata('warning'))) { ?>
+
+            <div class="alert alert-warning">
+                <?php echo session()->getFlashdata('warning'); ?>
+            </div>
+
+        <?php } ?>
+    </div>
+    <div class="container">
+
+        <div class="table-responsive">
             <table class="table table-bordered">
-                <tr>
+                <thead>
                     <th>Tanggal Update</th>
                     <th>Nama Bahan</th>
                     <th>Sub Bahan</th>
@@ -30,27 +52,29 @@
                     <th>Stok Awal</th>
                     <th>Masuk</th>
                     <th>Keluar</th>
+                    <th>Akhir</th>
                     <th>Action</th>
-
-                </tr>
-                <?php foreach ($stok as $row) : ?>
-                    <tr>
-
-                        <td><?= $row['updateDate']; ?></td>
-                        <td><?= $row['nama']; ?></td>
-                        <td><?= $row['subBahan']; ?></td>
-                        <td><?= $row['satuan']; ?></td>
-                        <td><?= $row['stokAwal']; ?></td>
-                        <td><?= $row['stokMasuk']; ?></td>
-                        <td><?= $row['stokKeluar']; ?></td>
-                        <td>
-                            <a href='#' class="btn btn-primary btn-sm btn-edit" data-updateDate="<?= $row['updateDate']; ?>" data-stokAwal="<?= $row['stokAwal']; ?>" data-stokMasuk="<?= $row['stokMasuk']; ?>" data-stokKeluar="<?= $row['stokKeluar']; ?>">Edit</a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-
-                <!--end fungsi read data-->
-                <!--fungsi search-->
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($stok as $key => $data) { ?>
+                        <tr>
+                            <td><?php echo $data['updateDate']; ?></td>
+                            <td><?php echo $data['nama']; ?></td>
+                            <td><?php echo $data['subBahan']; ?></td>
+                            <td><?php echo $data['satuan']; ?></td>
+                            <td><?php echo $data['stokAwal']; ?></td>
+                            <td><?php echo $data['stokMasuk']; ?></td>
+                            <td><?php echo $data['stokKeluar']; ?></td>
+                            <td><?php echo $data['stokAkhir']; ?></td>
+                            <td>
+                                <div class="btn-group">
+                                    <a href="<?php echo ('http://localhost:8081/stokbahan/edit/' . $data['id']); ?>" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
                 <form method="get" action="<?php echo ('http://localhost:8081/stokbahan/pencarian/') ?>">
                     <div class="col-md-2 col-sm-2 col-xs-2" align="center">Nama Bahan
 
@@ -64,77 +88,12 @@
                     <br><input type="submit" class="btn btn-primary" value="Cari">
                 </form>
             </table>
-            <!--end fungsi search-->
-            <!--fungsi update-->
-            <form action="<?php echo ('http://localhost:8081/stokbahan/update/') ?>" method="post">
-                <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Edit Bahan</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="form-group">
-                                    <label>Tanggal Update</label>
-                                    <input type="date" class="form-control updateDate" name="updateDate">
-                                </div>
+        </div>
+    </div>
 
-                                <div class="form-group">
-                                    <label>Stok Awal</label>
-                                    <input type="number" class="form-control stokAwal" name="stokAwal">
-                                </div>
-
-                                <div class="form-group">
-                                    <label>Stok Masuk</label>
-                                    <input type="text" class="form-control stokMasuk" name="stokMasuk">
-                                </div>
-
-                                <div class="form-group">
-                                    <label>Stok Keluar</label>
-                                    <input type="text" class="form-control stokKeluar" name="stokKeluar">
-                                </div>
-
-                            </div>
-                            <div class="modal-footer">
-                                <input type="hidden" name="id" class="id">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary">Update</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-
-            <script>
-                $(document).ready(function() {
-                    $('.btn-edit').on('click', function() {
-                        // get data from button edit
-                        const id = $(this).data('id');
-                        const updateDate = $(this).data('updateDate');
-                        const stokAwal = $(this).data('stokAwal');
-                        const stokMasuk = $(this).data('stokMasuk');
-                        const stokKeluar = $(this).data('stokKeluar');
-
-                        // Set data to Form Edit
-                        $('.id').val(id);
-                        $('.updateDate').val(updateDate);
-                        $('.stokAwal').val(stokAwal);
-                        $('.stokMasuk').val(stokMasuk);
-                        $('.stokKeluar').val(stokKeluar);
-                        // Call Modal Edit
-                        $('#editModal').modal('show');
-                    });
-                });
-            </script>
-
-            <!--end-->
-
-
-
-
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
